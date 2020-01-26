aafaf8dc8efb4548c8872613a3ef048a7a1ea366
殷海翔 <yin.hx@burgeon.cn>
2020-01-26 13:29:21 +0800

    接入mybatisplus

--- src/main/java/com/sean/dianping/DianpingApplication.java
+++ src/main/java/com/sean/dianping/DianpingApplication.java
@@ -1,9 +1,11 @@
 package com.sean.dianping;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sean.dianping.mapper")
 public class DianpingApplication {
 
 	public static void main(String[] args) {
