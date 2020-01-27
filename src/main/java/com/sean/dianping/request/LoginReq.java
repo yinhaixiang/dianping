69dd4a0fbf91cc35b641919d352beb42e734867f
殷海翔 <yin.hx@burgeon.cn>
2020-01-27 14:30:03 +0800

    去除无用代码

--- src/main/java/com/sean/dianping/request/LoginReq.java
+++ src/main/java/com/sean/dianping/request/LoginReq.java
@@ -1,29 +1,21 @@
 package com.sean.dianping.request;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
 import javax.validation.constraints.NotBlank;
 
 /**
  * @author yinhaixiang
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
 public class LoginReq {
     @NotBlank(message = "手机号不能为空")
     private String telphone;
     @NotBlank(message = "密码不能为空")
     private String password;
 
-    public String getTelphone() {
-        return telphone;
-    }
-
-    public void setTelphone(String telphone) {
-        this.telphone = telphone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/sean/dianping/request/RegisterReq.java
+++ src/main/java/com/sean/dianping/request/RegisterReq.java
@@ -1,13 +1,19 @@
 package com.sean.dianping.request;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 /**
  * @author yinhaixiang
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
 public class RegisterReq {
-
     @NotBlank(message = "手机号不能为空")
     private String telphone;
     @NotBlank(message = "密码不能为空")
@@ -18,35 +24,4 @@ public class RegisterReq {
     @NotNull(message = "性别不能为空")
     private Integer gender;
 
-    public String getTelphone() {
-        return telphone;
-    }
-
-    public void setTelphone(String telphone) {
-        this.telphone = telphone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public Integer getGender() {
-        return gender;
-    }
-
-    public void setGender(Integer gender) {
-        this.gender = gender;
-    }
 }
